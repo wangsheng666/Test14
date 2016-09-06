1a9a041a6db24d5789a6e4e64c0416a37c5987ee
wangsheng666 <1028742881@qq.com>
2016-09-06 08:28:44 +0800

    掌握使用Intent隐式启动的方法。

---
+++ app/src/main/java/com/example/a123/test14/AnotherActivity.java
@@ -0,0 +1,13 @@
+package com.example.a123.test14;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by 123 on 2016/9/6.
+ */
+public class AnotherActivity extends AppCompatActivity {
+    protected void onCreated(Bundle savedInstanceState){
+        super.onCreate( savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
